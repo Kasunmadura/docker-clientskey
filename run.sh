b8523c2bc4415cc25a22ed384c05114239f5ccd3
kasunmaduraeng <kasun@fishpond.co.nz>
2017-12-18 15:49:37 +1300

    typose

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@
 docker pull kasunmadura/clientskeyapi:v1
 docker pull mongo
 docker network create --subnet 10.0.1.0/24 --gateway 10.0.1.1 clientskey
-docker run  -id -p 27017:27017 --name clientskeymongo --network clientskey --ip 10.0.1.101 --name mongodb --add-host='appapi.ck.com:10.0.1.102' --add-host='app.clientskey.com:10.0.1.103' --add-host='db.ck.com:10.0.1.101' mongo:latest
-docker run  -id -p 3333:5000 -v /root/test/server:/app --network clientskey --ip 10.0.1.102 --name appapi --add-host='appapi.ck.com:10.0.1.102' --add-host='app.clientskey.com:10.0.1.103' --add-host='db.ck.com:10.0.1.101'  kasunmadura/clientskeyapi:v1
+docker run  -id -p 27017:27017 --name clientskeymongo --network clientskey --ip 10.0.1.101 --name mongodb --add-host='appapi.ck.com:10.0.1.102' --add-host='app.ck.com:10.0.1.103' --add-host='db.ck.com:10.0.1.101' mongo:latest
+docker run  -id -p 3333:5000 -v /root/test/server:/app --network clientskey --ip 10.0.1.102 --name appapi --add-host='appapi.ck.com:10.0.1.102' --add-host='app.ck.com:10.0.1.103' --add-host='db.ck.com:10.0.1.101'  kasunmadura/clientskeyapi:v1
 docker run  -id -p 8080:8080 -v /root/test/clientskey_prod_v0.1/:/app --network clientskey --ip 10.0.1.103 --name app --add-host='appapi.ck.com:10.0.1.102' --add-host='app.ck.com:10.0.1.103' --add-host='db.ck.com:10.0.1.101'  kasunmadura/clientskeyapp:v2
