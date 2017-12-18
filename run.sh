1742f144d797d1e7ee7e3e7f5162e163216e4d23
kasunmaduraeng <kasun@fishpond.co.nz>
2017-12-18 15:38:11 +1300

    adding clientskey docker files

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Author Kasun Madura Rathanyaka
+## email : kasunmaduraeng@gmail.com
+
+## please check the db-connection json and check the db connection string to 10.0.1.101:27017
+## check the file location where appapi repo located in you server/PC
+docker pull kasunmadura/clientskeyapi:v1
+docker pull mongo
+docker network create --subnet 10.0.1.0/24 --gateway 10.0.1.1 clientskey
+docker run  -id -p 27017:27017 --name clientskeymongo --network clientskey --ip 10.0.1.101 --name mongodb --add-host='appapi.clientskey.com:10.0.1.102' --add-host='app.clientskey.com:10.0.1.103' --add-host='db.clientskey.com:10.0.1.101' mongo:latest
+docker run  -id -p 3333:5000 -v /root/test/server:/app --network clientskey --ip 10.0.1.102 --name appapi --add-host='appapi.clientskey.com:10.0.1.102' --add-host='app.clientskey.com:10.0.1.103' --add-host='db.clientskey.com:10.0.1.101'  kasunmadura/clientskeyapi:v1
+docker run  -id -p 8080:8080 -v /root/test/clientskey_prod_v0.1/:/app --network clientskey --ip 10.0.1.103 --name app --add-host='appapi.clientskey.com:10.0.1.102' --add-host='app.clientskey.com:10.0.1.103' --add-host='db.clientskey.com:10.0.1.101'  kasunmadura/clientskeyapp:v2
